d09b4052a02c113412ebfc5d24cf16d3df83fec7
Wesley Obenshain <wesley.obenshain@nci.nih.gov>
2015-09-10 16:48:26 -0400

    Created basic node.js express server

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var expressLogger = require('express-logger');
+var app = express();
+
+app.use(expressLogger({ path: "express.log" }));
+app.use(express.static("static"));
+
+// Default values
+var port = 8080;
+
+function parseArgs() {
+	var usage = false;
+	for (var index = 2; index < process.argv.length; index++) {
+		switch (process.argv[index]) {
+			case "-p":
+				index++;
+				port = process.argv[index];
+				if (typeof value !== "number") break;
+			default:
+				usage = true;
+		}
+		if (usage) break;
+	}
+	if (usage) {
+		console.log("node server.js [-p <port>]");
+		process.exit(1);
+	}
+}
+
+parseArgs();
+var server = app.listen(port, function() {});
