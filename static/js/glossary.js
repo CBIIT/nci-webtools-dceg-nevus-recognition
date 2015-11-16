49222d5585f6f6031e82693600297bc48d75ee4b
Wesley Obenshain <wesley.obenshain@nci.nih.gov>
2015-11-16 17:11:09 -0500

    begin glossification

---
+++ static/js/glossary.js
@@ -0,0 +1,35 @@
+$.extend($_Glossary,{
+  "": {
+    "fullName": "",
+    "definition": ""
+  },
+  "amole": {
+    "fullName": "Atypical Mole",
+    "definition": "A type of nevus (mole) that looks different from a common mole. An atypical mole is often larger with borders that are not easy to see. Its color is usually uneven and can range from pink to dark brown. Parts of the mole may be raised above the skin surface. An atypical mole may develop into malignant melanoma (a type of skin cancer). Also called dysplastic nevus."
+  },
+  "nevus": {
+    "fullName": "Dysplastic Nevus",
+    "definition": "A type of nevus (mole) that looks different from a common mole. A dysplastic nevus is often larger with borders that are not easy to see. Its color is usually uneven and can range from pink to dark brown. Parts of the mole may be raised above the skin surface. A dysplastic nevus may develop into malignant melanoma (a type of skin cancer). Also called atypical mole."
+  },
+  "epidemiologist": {
+    "fullName": "Epidemiologist",
+    "definition": "A scientist who studies the patterns, causes, and control of disease in groups of people. "
+  },
+  "lesion": {
+    "fullName": "lesion",
+    "definition": "An area of abnormal tissue. A lesion may be benign (not cancer) or malignant (cancer)."
+  },
+  "marker": {
+    "fullName": "Marker",
+    "definition": "A diagnostic indication that disease may develop."
+  },
+  "melanoma": {
+    "fullName": "Melanoma",
+    "definition": "A form of cancer that begins in melanocytes (cells that make the pigment melanin). It may begin in a mole (skin melanoma), but can also begin in other pigmented tissues, such as in the eye or in the intestines."
+  },
+  "mole": {
+    "fullName": "Mole",
+    "definition": "A benign (not cancer) growth on the skin that is formed by a cluster of melanocytes (cells that make a substance called melanin, which gives color to skin and eyes). A mole is usually dark and may be raised from the skin. Also called nevus."
+  }
+});
+$(document).on('click touchstart','.define',termDisplay);
