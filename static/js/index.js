e15f614564a4cb790cc413ed9cb485571c88d9b9
Wesley Obenshain <wesley.obenshain@nih.gov>
2016-02-17 16:06:55 -0500

    NCIATWP-328

--- static/js/index.js
+++ static/js/index.js
@@ -139,6 +139,7 @@ app.controller('myCtrl', function($rootScope, $scope, $http, $timeout) {
     $scope.searching = false;
     $scope.ulinks = false;
     $scope.disclaimer = false;
+    $('body,html').animate({scrollTop:0}, 100);
   };
   $scope.clearsearch = function() {
     $scope.search = "";
@@ -185,7 +186,10 @@ app.controller('myCtrl', function($rootScope, $scope, $http, $timeout) {
       'page': 'about'
     }, 'About the Tool');
   };
-  $scope.goaudience = function() {
+  $scope.goaudience = function(e) {
+    if (e !== undefined) {
+      e.stopPropagation();
+    }
     $scope.changePage();
     $scope.audience = true;
     newState({
@@ -206,7 +210,10 @@ app.controller('myCtrl', function($rootScope, $scope, $http, $timeout) {
       $scope.filterClick($scope.getFilterByName('mole'));
     }
   };
-  $scope.goulinks = function() {
+  $scope.goulinks = function(e) {
+    if (e !== undefined) {
+      e.stopPropagation();
+    }
     $scope.changePage();
     $scope.ulinks = true;
     newState({
