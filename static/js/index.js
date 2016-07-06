0b82f7912bb9985cf0a65d07a15435566eacbd3c
Wesley Obenshain <wesley.obenshain@nih.gov>
2016-07-06 13:23:22 -0400

    added comments

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+// convenience method for single-page apps with usable links
 function newState(urlParams, title) {
     var url = "";
     for (var index in urlParams) {
@@ -5,14 +6,17 @@ function newState(urlParams, title) {
     }
     url.length == 0 ? void(0) : url = '?' + url.substring(1, url.length);
     try {
+        // If history state doesn't work with this browser there's nothing we can do to make it work short of reloading the page. Ignore the problem and move on.
         window.history.pushState == undefined ? void(0) : window.history.pushState(urlParams, "Moles, Dysplastic Nevi & Melanoma - " + title, window.location.pathname + url);
     } catch (e) {}
 }
 
 $(document).ready(function () {
+    // Principle understands that disabling right click on images won't actually prevent people from downloading them but wants it done anyway
     $('body, html').on('contextmenu', 'img', function (event) {
         event.preventDefault();
     });
+    // When case modal is hidden, reset current URL to reflect this
     $('#case').on('hidden.bs.modal', function () {
         var urlParams = window.history.state || {};
         delete urlParams.case;
@@ -28,6 +32,7 @@ $(document).ready(function () {
     */
     
     $("[role='tab'], .partial-collapse").on("click keypress", function (e) {
+        // Used to allow enter and space to trigger AngularJS click functions without additional Angular code.
         if (e.keyCode == 13 || e.keyCode == 32) {
             e.preventDefault();
             $(e.target).trigger('click');
@@ -65,6 +70,7 @@ $(document).ready(function () {
     $('.app').on('click', '.blur', function () {
         $(this).blur();
     });
+    // Used to allow enter and space to trigger AngularJS click functions without additional Angular code.
     $(document).on('keypress', '[tabindex][ng-click], button[data-dismiss="modal"]', function (e) {
         if (e.keyCode == 13 || e.keyCode == 32) {
             e.preventDefault();
@@ -76,6 +82,7 @@ $(document).ready(function () {
 var app = angular.module('myApp', ['ngSanitize']);
 
 app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
+    // Because this is a single-page app, we need to use query parameters to determine what part of the page we're trying to access
     var loadFunction = function () {
         var match, pl = /\+/g, // Regex for replacing addition symbol with a space
             search = /([^&=]+)=?([^&]*)/g,
@@ -146,6 +153,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             }
         });
     };
+    // Angular is awful. Case in point, the next two pages of mostly-useless variables just to make the "magic" work.
     var pages = 1,
         itemsperpage = 6,
         numitems = 0,
@@ -159,9 +167,16 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         }
     };
     var $container = $('.isotope');
+    // This and the isotope library are where most of the magic happens
+    // This actually kind of an awful way to do things. It WILL break with a larger data set
+    // But time constraints prevented a complete rewrite of what I was handed
     var basicFilter = function (trueFilter) {
         return function () {
+            // actual filter is passed in by the calling function
+            // which should either be someone clicking on one of the
+            // filter links, or typing something in the search box
             if (trueFilter($(this))) {
+                // Handles page load limitations
                 if (numitems < itemsperpage * pages) {
                     numitems++;
                     return true;
@@ -172,9 +187,12 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         };
     };
     var isotopic = function (trueFilter) {
+        // This little hack lets us use one block of code to handle both the searching and display methods with minimal redundant code
         trueFilter = typeof trueFilter === undefined ? function (element) {
             return true;
         } : trueFilter;
+        // if we try to manipulate stuff from within an Angular heartbeat using non-Angular methodologies, it breaks the site. Literally. The whole site.
+        // using $timeout lets it happen outside the Angular heartbeat so we can use isotope to handle panel arrangement
         $timeout(function () {
             $container.isotope({
                 itemSelector: '.item',
@@ -182,28 +200,28 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             });
         });
     };
-    $scope.cases = data.cases;
-    $scope.filters = data.filters;
-    $scope.about = false;
-    $scope.audience = false;
-    $scope.currentcase = {};
-    $scope.currentimg = 0;
-    $scope.currenttype = $scope.filters[0];
-    $scope.filterValue = '';
-    $scope.home = true;
-    $scope.searching = false;
-    $scope.tool = false;
-    $scope.ulinks = false;
-    $scope.showbacktotop = false;
-    $scope.showload = false;
-    $scope.search = "";
-    $scope.subgrouptype = null;
-    $scope.backToTop = function () {
+    $scope.cases = data.cases; // case data
+    $scope.filters = data.filters; // filter data
+    $scope.about = false; // display about page?
+    $scope.audience = false; // display audience page?
+    $scope.currentcase = {};  // currently open case from case data
+    $scope.currentimg = 0; // currently selected image in the currently open case
+    $scope.currenttype = $scope.filters[0]; // filter currently being applied
+    $scope.filterValue = ''; // ... because otherwise we have to put substantial code into the HTML.
+    $scope.home = true; // display home page? default answer: yes
+    $scope.searching = false; // Are we currently using the search function?
+    $scope.tool = false; // display tool (meat of the app) page?
+    $scope.ulinks = false; // display additional information page?
+    $scope.showbacktotop = false; // Angular would rather I set a variable than use CSS or responsive Javascript
+    $scope.showload = false; // As above
+    $scope.search = ""; // Holds input from search box
+    $scope.subgrouptype = null; // there's more than one kind of filter
+    $scope.backToTop = function () { // if the function isn't declared as $scope, it can't be called from Angular's HTML code
         $('body,html').animate({
             scrollTop: 0
         }, 1000);
     };
-    $scope.changePage = function () {
+    $scope.changePage = function () { // convenience method to reset the visual appearance before we open a new page
         $scope.showfull = false;
         $scope.about = false;
         $scope.audience = false;
@@ -221,9 +239,9 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
     };
     $scope.clearsearch = function () {
         $scope.search = "";
-        $scope.filterClick($scope.getFilterByName('mole'), true);
+        $scope.filterClick($scope.getFilterByName('mole'), true); // canceling search defaults to "Moles" section fo the cases
     };
-    $scope.getFilterByName = function (filterName) {
+    $scope.getFilterByName = function (filterName) { // Again, we need do things this way to avoid putting too much code in the Angular HTML
         for (var index = 0; index < $scope.filters.length; index++) {
             var filter = $scope.filters[index];
             if (filter.type == filterName) {
@@ -232,6 +250,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         }
         return $scope.filters[0];
     }
+    // There's a lot of actual work that goes into the 'magic' filters
     $scope.filterClick = function (filter, subgroup, fromHistory) {
         var urlParams = window.history.state || {};
         urlParams.page = 'tool';
@@ -261,6 +280,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             newState(urlParams, 'Recognition Tool');
         isotopic(filterFns.by6);
     }
+    // Because Angular requires a whole bunch of JS code to work around all the problems its 'magic' creates
     $scope.goabout = function () {
         $scope.changePage();
 
@@ -271,6 +291,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             'page': 'about'
         }, 'About the Tool');
     };
+    // Because Angular...
     $scope.goaudience = function (e) {
         if (e !== undefined) {
             e.stopPropagation();
@@ -285,6 +306,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             'page': 'audience'
         }, 'Intended Audience');
     }
+    // Because Angular...
     $scope.gohome = function () {
         $scope.changePage();
         $("[aria-controls='home']").addClass('active');
@@ -294,6 +316,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             'page': 'home'
         }, 'Home');
     };
+    // Because Angular...
     $scope.gotool = function () {
         $scope.changePage();
         $("[aria-controls='cases']").addClass('active');
@@ -303,6 +326,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             $scope.filterClick($scope.getFilterByName('mole'));
         }
     };
+    // Because Angular...
     $scope.goulinks = function (e) {
         if (e !== undefined) {
             e.stopPropagation();
@@ -316,6 +340,7 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             'page': 'ulinks'
         }, 'Useful Links');
     };
+    // Because Angular...
     $scope.godisclaimer = function () {
         $scope.changePage();
 
@@ -327,22 +352,27 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
             'page': 'disclaimer'
         }, 'Disclaimer');
     };
+    // Because Angular...
     $scope.loadMore = function () {
         pages++;
         $scope.showload = false;
         numitems = 0;
         isotopic($scope.tool ? filterFns.by6 : filterFns.searchby6);
     }
+    // Because Angular...
     $scope.scrollLeft = function () {
         $('.images').animate({
             scrollLeft: $('.images').scrollLeft() - $('.images').width()
         }, 300);
     }
+    // Because Angular...
     $scope.scrollRight = function () {
         $('.images').animate({
             scrollLeft: $('.images').scrollLeft() + $('.images').width()
         }, 300);
     }
+    // Because Angular...
+    // triggered by typing in the search box
     $scope.searchingfunc = function () {
         if ($scope.search == undefined) {
             return $scope.clearsearch();
@@ -360,6 +390,8 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         }, 'Recognition Tool');
         isotopic(filterFns.searchby6);
     }
+    // Because Angular...
+    // opens case modal with proper data
     $scope.update = function (caseObject, index) {
         $scope.currentcase = caseObject;
         var urlParams = window.history.state || {};
@@ -367,6 +399,8 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         newState(urlParams, 'Recognition Tool');
         $scope.updatecurrentimg(0);
     };
+    // Because Angular... *sigh*
+    // updates case modal's data based on selected image
     $scope.updatecurrentimg = function (index) {
         $scope.currentimg = index;
         var urlParams = window.history.state || {};
@@ -375,19 +409,28 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
         index++;
         $('.spinnerbox').addClass('spinner-show');
     };
+    // There are lots of things Angular can't handle or doesn't handle well. Like responsive Javascript.
+    // Specifically, in this case, responding to down a 'page'.
     $(window).scroll(function () {
+        // This is kind of the inverse of $timeout. $scope.$apply forces the function to occur within Angular's heartbeat framework
+        // Without this, the variables don't update properly and even if they did the UI changes wouldn't be triggered
         $scope.$apply(function () {
             $scope.showbacktotop = $(window).scrollTop() > 500;
         });
     });
+    // responds to 'new page' events such as using the forward and back buttons on the browser
     $(window).bind('popstate', function () {
         loadFunction();
     });
+    // Only triggers when one navigates to the URL. Not triggered by forward and back buttons
     $(window).load(function () {
         loadFunction();
         $timeout(function () {
             $('#disclaimerModal').modal('show');
         });
+        // I'm not actually sure why this is here and not with other, similar functions,
+        // but I probably tried and it didn't work for one reason or another. onLoad
+        // is as good a place as any, anyway.
         $('#case').on('swipeleft', '.currentimage', function () {
             $scope.$apply(function () {
                 $scope.updatecurrentimg(Math.min($scope.currentimg + 1, $scope.currentcase.images.length - 1));
@@ -400,6 +443,8 @@ app.controller('myCtrl', function ($rootScope, $scope, $http, $timeout) {
     });
 });
 
+// This allows our data to contain HTML and, specifically, AngularJS directives
+// This should never be applied to anything with user-submitted content
 app.directive('bindCompiledHtml', function ($compile) {
     return function (scope, element, attrs) {
         scope.$watch(
@@ -427,6 +472,7 @@ app.directive('imageOnLoad', function () {
     };
 });
 
+// makes the image carousel work properly in Angular
 app.directive('carouselDir', function () {
     return function (scope, element, attrs) {
         if (scope.$last) {
@@ -455,6 +501,8 @@ app.directive('carouselDir', function () {
     };
 });
 
+// filters allow us to feed data through a function before it shows up on the front end
+// This theoretically one causes the first letter of each word to be capitalized.
 app.filter('titleCase', function () {
     return function (input) {
         output = input || '';
@@ -464,6 +512,9 @@ app.filter('titleCase', function () {
     };
 })
 
+// This one automatically picks out (very specific) cancer terms from our glossary
+// and wraps them in the proper context to allow of definition popups to work
+// This would break down with a larger glossary but it works for now
 app.filter('define', function () {
     return function (input) {
         output = input || '';
