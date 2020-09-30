44e0fe24cc20b2e3b1040a4150dc958ad2be5203
Brian Park <park-brian@users.noreply.github.com>
2020-09-29 20:36:31 -0400

    add analytics snippet

---
+++ static/js/analytics.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-62346354-17');
+_satellite && _satellite.pageBottom();
