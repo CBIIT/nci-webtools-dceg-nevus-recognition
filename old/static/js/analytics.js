9792b5ebb5351b17210e073b1f6af11a52db6506
Brian Park <park-brian@users.noreply.github.com>
2023-10-18 12:43:10 -0400

    remove deprecated code

--- old/static/js/analytics.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-62346354-17');
-_satellite && _satellite.pageBottom();
